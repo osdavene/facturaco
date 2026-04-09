a5fe03feba4fd64ad8831ddc4718fdec62d9dcb1
osdavene <vegan.oscar@gmail.com>
2026-04-09 09:50:10 -0500

    debug variables entorno en docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,9 +3,16 @@ set -e
 
 cd /var/www/html
 
-# Limpiar cache de configuración
+echo "=== Variables de entorno ==="
+echo "DB_HOST: $DB_HOST"
+echo "DB_DATABASE: $DB_DATABASE"
+echo "DB_USERNAME: $DB_USERNAME"
+
+# Limpiar todo el cache
 php artisan config:clear
 php artisan cache:clear
+php artisan view:clear
+php artisan route:clear
 
 # Ejecutar migraciones
 php artisan migrate --force
