706985959d7bfe3d341387732eda9437c9df92b6
osdavene <vegan.oscar@gmail.com>
2026-08-21 11:52:07 -0500

    Remover middleware verified de la ruta dashboard

--- routes/web/dashboard.php
+++ routes/web/dashboard.php
@@ -4,5 +4,4 @@ use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/dashboard', [DashboardController::class, 'index'])
-    ->middleware('verified')
     ->name('dashboard');
