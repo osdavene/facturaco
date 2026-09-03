a461bc42a6a8a3f6f70c88c4e97ab382abcc4464
osdavene <vegan.oscar@gmail.com>
2026-09-02 20:20:53 -0500

    Restaurar ruta DELETE backoffice.usuarios.destroy

--- routes/web/backoffice.php
+++ routes/web/backoffice.php
@@ -20,6 +20,7 @@ Route::middleware(['auth', 'backoffice'])->prefix('backoffice')->name('backoffic
     Route::get('/usuarios',                        [BackofficeController::class, 'usuariosIndex'])  ->name('usuarios');
     Route::get('/usuarios/{usuario}/editar',       [BackofficeController::class, 'usuarioEditar'])  ->name('usuarios.editar');
     Route::put('/usuarios/{usuario}',              [BackofficeController::class, 'usuarioUpdate'])  ->name('usuarios.update');
+    Route::delete('/usuarios/{usuario}',           [BackofficeController::class, 'usuarioDestroy']) ->name('usuarios.destroy');
     Route::get('/backup',                          [BackofficeController::class, 'backupIndex'])     ->name('backup');
     Route::get('/backup/descargar',                [BackofficeController::class, 'backupDescargar']) ->name('backup.descargar');
     Route::get('/dian',                            [BackofficeController::class, 'dianIndex'])       ->name('dian');
